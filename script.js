f17b5580aadd7dae2a851e9140c8e868934df191
cciacomo <esposito.giacomo2008@gmail.com>
2024-10-27 20:58:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var display = document.getElementById("display");
+var menuButton = document.getElementById("menu-button");
+var removeMenu = document.getElementById("menu");
+var balance = document.getElementById("balance-num");
+
+const onLoad = () => {
+  menuButton.classList.add("onscreen");
+  removeMenu.classList.add("remove");
+  showBalance();
+};
+window.onload = onLoad;
+
+const menuInteraction = () => {
+  if (display.classList.contains("blur")) {
+    display.classList.remove("blur");
+    removeMenu.classList.remove("onscreen");
+    removeMenu.classList.add("remove");
+    menuButton.classList.add("onscreen");
+    menuButton.classList.remove("remove");
+  } else {
+    display.classList.add("blur");
+    removeMenu.classList.remove("remove");
+    removeMenu.classList.add("onscreen");
+    menuButton.classList.add("remove");
+    menuButton.classList.remove("onscreen");
+  }
+};
+
+const showBalance = () => {
+  if (balance.classList.contains("blur")) {
+    balance.classList.remove("blur");
+  } else {
+    balance.classList.add("blur");
+  }
+};
+
+balance.onclick = showBalance;
